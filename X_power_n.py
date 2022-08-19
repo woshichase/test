62a64336f0796e65954f32b2a89b91e819aa3122
Eric <woshichase@gmail.com>
2022-08-19 18:24:23 +0800

    first commit

---
+++ X_power_n.py
@@ -0,0 +1,26 @@
+# n is an integer
+
+def x_power_n (x,n):
+    if n==0:
+        return 1
+    else:
+        abs_n = abs(n)
+        s = 1
+        while abs_n > 0:
+            s = s * x
+            abs_n = abs_n-1
+        if n > 0:
+            return s
+        else:
+            return (1/s)
+
+if __name__ == '__main__':
+    print(x_power_n(3, 3))
+    print(x_power_n(3, 0))
+    print(x_power_n(3, -2))
+    # import matplotlib.pyplot as plt
+    # import matplotlib
+    # matplotlib.use('TKAgg')  #
+    # plt.plot([1, 2], [2, 1], 'k')
+    # plt.figure(1)
+    # plt.plot([1, 2], [2, 1], 'k')
